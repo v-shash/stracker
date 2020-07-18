adb76a4663ef078d4a67c0c4c3f0729a7a2b29d4
Mo Husseini <mhusseini1208@gmail.com>
2020-07-18 15:54:45 -0400

    Fix deployment issues
    	- Fix settings.py
    	- Fix debug environment variables

--- backend/services/stock_service/stock_service/settings.py
+++ backend/services/stock_service/stock_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = False
 
